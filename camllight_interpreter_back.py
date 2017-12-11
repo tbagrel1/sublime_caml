4ba8bb3f4bae7940b020a3bb3106cec796ac2147
Thomas BAGREL <tomsb07@gmail.com>
2017-12-11 20:56:05 +0100

    corrections v2

--- camllight_interpreter_back.py
+++ camllight_interpreter_back.py
@@ -15,52 +15,55 @@ GIT_LINK = ("https://raw.githubusercontent.com/tbagrel1/" +
 ERROR_MSG = "\n=== ERRORS ===\n"
 SCRIPT_PATH = sys.argv[0]
 CLEAN = True
+ENC = "utf-8"
 
 def update(debug):
     """Update the current script by downloading the new one from GitHub."""
     # Downloading
     try:
         subprocess.run("cd \"{}\" && wget \"{}\"".format(TEMP_DIR, GIT_LINK),
-                       shell=True, check=True, stderr=subprocess.PIPE)
+                       shell=True, check=True, stderr=subprocess.PIPE,
+                       encoding=ENC)
     except subprocess.CalledProcessError as e:
         click.echo("\nUnable to retrieve the new version of the script. "
-                   "Please try again later.\n")
+                   "Please try again later.")
         if debug:
-            click.echo("Original error: [{}]\n".format(e.stderr.read()))
+            click.echo("Original error: [{}]".format(e.stderr))
         return False
     # Move (may need superuser privileges)
     try:
         subprocess.run("cd {} && mv \"camllight_interpreter.py\" \"{}\""
                        .format(TEMP_DIR, SCRIPT_PATH), shell=True, check=True,
-                       stderr=subprocess.PIPE)
+                       stderr=subprocess.PIPE, encoding=ENC)
     except subprocess.CalledProcessError as e:
         click.echo("\nUnable to move the new script to the current script "
                    "directory. Please re-run the script with enough "
-                   "privileges.\n")
+                   "privileges.")
         if debug:
-            click.echo("Original error: [{}]\n".format(e.stderr.read()))
+            click.echo("Original error: [{}]".format(e.stderr))
         return False
-    click.echo("\nUpdate was successful!\n")
+    click.echo("\nUpdate was successful!")
     return True
 
 def clean_stdout(text):
     """Clean the stdout given by the Caml Light interpreter."""
-    return "\n".join([line[1:] for line in text.split("\n")[2:]]) + "\n"
+    return "\n".join([line.strip("#") for line in text.split("\n")[2:]])
 
 def clean_stderr(text):
     """Clean the stderr given by the Caml Light interpreter."""
-    return "\n".join([line[1:] for line in text.split("\n")]) + "\n"
+    return text
 
-def run(in_path, out_file, show, debug):
+def camlrun(in_path, out_file, show, debug):
     """Run the specified Caml Light file."""
     try:
         obj = None
-        with open(in_path, "r", encoding="utf-8") as in_file:
+        with open(in_path, "r", encoding=ENC) as in_file:
             obj = subprocess.run(["camllight"], stdin=in_file,
                                  stdout=subprocess.PIPE,
-                                 stderr=subprocess.PIPE)
-        stdout = obj.stdout.read()
-        stderr = obj.stderr.read()
+                                 stderr=subprocess.PIPE,
+                                 encoding=ENC)
+        stdout = obj.stdout
+        stderr = obj.stderr
         if CLEAN:
             stdout = clean_stdout(stdout)
             stderr = clean_stderr(stderr)
@@ -73,9 +76,9 @@ def run(in_path, out_file, show, debug):
             click.echo(result)
         return True
     except Exception as e:
-        click.echo("\nUnexpected error happened.\n")
+        click.echo("\nUnexpected error happened.")
         if debug:
-            click.echo("Original error: [{}]\n".format(e))
+            click.echo("Original error: [{}]".format(e.decode))
         return False
 
 @click.command()
@@ -83,14 +86,23 @@ def run(in_path, out_file, show, debug):
                 type=click.Path(
                     exists=True, file_okay=True, dir_okay=False, readable=True,
                     resolve_path=True))
-@click.option("--out-file", "-o", type=click.File(mode="w", encoding="utf-8"))
-@click.option("--run/--update", default=True)
-@click.option("--show/--no-show", default=True)
-@click.option("--debug/--no-debug", default=False)
+@click.option("--out-file", "-o", type=click.File(mode="w", encoding=ENC),
+              help="Optional file where output will be written.")
+@click.option("--run/--update", default=True,
+              help="Run the specified file or update this script.")
+@click.option("--show/--hide", default=True,
+              help="Show or hide the Caml Light output.")
+@click.option("--debug/--no-debug", default=False,
+              help="Show or hide debug information.")
 def main(in_path, out_file, run, show, debug):
-    """Launcher."""
+    """Caml Light interpreter written in Python 3 in order to get the output of
+    Caml Light programs as it was launched with the official interpreter."""
     if not run:
         update(debug)
     else:
-        run(in_path, out_file, show, debug)
+        camlrun(in_path, out_file, show, debug)
 
+if __name__ == "__main__":
+    main()
+
+##
