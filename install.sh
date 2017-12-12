e79c57bada2b8d6f8778bce984c9828c5bc75384
Thomas BAGREL <tomsb07@gmail.com>
2017-12-12 13:59:01 +0100

    modif: all

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+apt-get install python3
+apt-get install python3-pip
+pip3 install click
+
 chmod +x ./camllight_interpreter
 cp ./camllight_interpreter /usr/bin/
+
+echo "Installation was successful!"
